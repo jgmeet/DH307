429ae1a738975d848cd26877aac86966018a14b2
jgmeet <119388448+jgmeet@users.noreply.github.com>
2023-11-29 14:39:31 +0530

    Adding pipeline 2

---
+++ Pipeline2/pipline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Step 1: Quality Control with FastQC
+#fastqc SRR10042628_1.fastq -o quality_check/
+echo "FastQC finished running."
+
+# Step 2: Trim Adapters and Low-Quality Reads with TrimGalore
+#trim_galore --quality 5 --length 36 --output_dir quality_check/ SRR10042628_1.fastq
+echo "TrimGalore finished running."
+
+# Step 3: Alignment with STAR
+STAR --genomeDir STAR_index --readFilesIn quality_check/SRR10042628_1_trimmed.fq --outFileNamePrefix align/SRR10042628_1_star
+echo "STAR alignment finished running."
